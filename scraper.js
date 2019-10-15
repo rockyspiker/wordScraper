42e5034dbc1a17d62a6d6e51a1f6b43b8b360045
Rocky <rockyspiker@gmail.com>
2019-10-15 12:11:32 -0600

    Working version, still needs cleaning

--- scraper.js
+++ scraper.js
@@ -2,24 +2,37 @@ const puppeteer = require('puppeteer')
 const fs = require('fs')
 
 const baseURL = 'https://www.passwordrandom.com/most-popular-passwords'
+let curURL = ''
 
 async function scrape() {
+    const allPasswords = []
+
     const browser = await puppeteer.launch()
 
     const page = await browser.newPage()
     console.log('Puppeteer Initialized')
 
-    await page.goto(baseURL)
+    for (let pageNum = 1; pageNum <= 100; pageNum++) {
+        if (pageNum > 1)
+            curURL = 'https://www.passwordrandom.com/most-popular-passwords/page/' + pageNum
+        else
+            curURL = baseURL
 
-    const tableRows = await page.evaluate(() => document.querySelectorAll("#cntContent_lstMain tr:not(:first-child) td:nth-child(2)"))
-    const talkText = []
-    for (let row in tableRows)
-        talkText.push(tableRows[row].textContent)
-    console.log(talkText)
+        await page.goto(curURL)
 
+        const pagePasswords = await page.evaluate(() => {
+            const elements = [...document.querySelectorAll("#cntContent_lstMain tr:not(:first-child) td:nth-child(2)")]
+            return elements.map(element => element.textContent)
+        })
+
+        for (let pwd in pagePasswords)
+            allPasswords.push(pagePasswords[pwd])
+
+            console.log('Finished page ', pageNum)
+    }
     await browser.close();
 
-    fs.writeFileSync('mcupws.json', JSON.stringify([...talkText]))
+    fs.writeFileSync('mcupws.json', JSON.stringify([...allPasswords]))
 
 }
 
